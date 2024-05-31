91a9e6c0eff087d10bd4e70242ad7370f8e621ca
HuangZixian <496201342@qq.com>
2024-05-31 18:22:48 +0800

    Add files via upload

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -1,3 +1,3 @@
 
-python run_evaluation.py --llm_path ../LLMs/MetaMath-7B-V1.0/ --mt_path ../LLMs/mt5-xl/ \
+python run_evaluation.py --llm_path meta-math/MetaMath-7B-V1.0 --mt_path google/mt5-xl \
        --init_checkpoint outputs/MergeMinds/augmentation/pytorch_model.bin --augmentation True
