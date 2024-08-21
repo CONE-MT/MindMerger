4b8234ad70455a7d1c27274187cdecdad23afb57
HuangZixian <496201342@qq.com>
2024-08-21 19:44:53 +0800

    Delete scripts directory

--- scripts/evaluation.sh
+++
@@ -1,3 +0,0 @@
-
-python run_evaluation.py --llm_path meta-math/MetaMath-7B-V1.0 --mt_path google/mt5-xl \
-       --init_checkpoint outputs/MindMerger/augmentation/pytorch_model.bin --augmentation True
